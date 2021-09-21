f6670420bdc399f0c611a03ae408f6b4b798b922
usrnatc <n.corcoran@uqconnect.edu.au>
2021-09-22 00:17:01 +1000

    alterred animated text

--- terminal.js
+++ terminal.js
@@ -11,7 +11,7 @@ var typewriter = new Typewriter(app, {
 typewriter
     .pauseFor(50)
     .typeString('#include "me.h"<br />')
-    .typeString('Person me = (Person) malloc(sizeof(Person));<br />')
+    .typeString('Person me = *(Person *) malloc(sizeof(Person));<br />')
     .typeString('me.age = 20;<br />')
     .typeString('me.uni = (char *) malloc(BUFFERSIZE * sizeof(char));<br />')
     .typeString('me.uni = \"University of Queensland\\n\";<br />')
