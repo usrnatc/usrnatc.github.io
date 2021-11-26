02352c7c5e586f17d279606bf874e3c6487bc76a
usrnatc <n.corcoran@uqconnect.edu.au>
2021-11-26 19:48:42 +1000

    fixed spelling error

--- terminal.js
+++ terminal.js
@@ -17,6 +17,6 @@ typewriter
     .typeString('me->langs = {\"Python\", \"Java\", \"C\", \"Haskell\", \"Dafny\", \"HTML\", \"CSS\"};<br />')
     .typeString('printf("%s\\n", me->info);<br />')
     .typeString('$ I am currently a third year software engineering student at UQ.<br />\
-		  &nbsp;&nbsp;I made this web-page to learn some simple front-end abailities with HTML, CSS, and trivial javascript.<br />\
+		  &nbsp;&nbsp;I made this web-page to learn some simple front-end abilities with HTML, CSS, and trivial javascript.<br />\
 		  &nbsp;&nbsp;If you have any queries, concerns, and/or improvements to this web-page let me know <br />&nbsp;&nbsp;in an email through the \'/contact\' link at the top of the page.<br /><br />\
 		  &nbsp;&nbsp;See my resume through the \'/about\' link at the top of the page for more info about me.<br /><br />')
