7f96f952e82d4a7b6e703a9af3ce15f85cdc0d2e
usrnatc <n.corcoran@uqconnect.edu.au>
2021-11-26 19:43:50 +1000

    updated email fixed styling issues

--- terminal.js
+++ terminal.js
@@ -18,5 +18,5 @@ typewriter
     .typeString('printf("%s\\n", me->info);<br />')
     .typeString('$ I am currently a third year software engineering student at UQ.<br />\
 		  &nbsp;&nbsp;I made this web-page to learn some simple front-end abailities with HTML, CSS, and trivial javascript.<br />\
-		  &nbsp;&nbsp;If you have any queries, concerns, and/or improvements to this web-page let me know <br />&nbsp;&nbsp;in an email through the contact link at the top of the page.<br /><br />\
+		  &nbsp;&nbsp;If you have any queries, concerns, and/or improvements to this web-page let me know <br />&nbsp;&nbsp;in an email through the \'/contact\' link at the top of the page.<br /><br />\
 		  &nbsp;&nbsp;See my resume through the \'/about\' link at the top of the page for more info about me.<br /><br />')
