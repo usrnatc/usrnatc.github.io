d5f0921d81975a7cfa572001726c19afbaad07f0
usrnatc <n.corcoran@uqconnect.edu.au>
2021-11-26 19:32:25 +1000

    added more bio text

--- terminal.js
+++ terminal.js
@@ -5,22 +5,18 @@ var typewriter = new Typewriter(app, {
         delay: 75,
         autoStart: true,
         cursor: '_',
-        strings: ['Nathan Corcoran links hub<br />I am testing this out.<br /><br />']
+	strings: ['']
 });
 
 typewriter
-    .pauseFor(50)
     .typeString('#include "me.h"<br />')
-    .typeString('Person me = *(Person *) malloc(sizeof(Person));<br />')
-    .typeString('me.age = 20;<br />')
-    .typeString('me.uni = (char *) malloc(BUFFERSIZE * sizeof(char));<br />')
-    .typeString('me.uni = \"University of Queensland\\n\";<br />')
-    .typeString('me.langs = {\"Python\", \"Java\", \"C\", \"Haskell\", \"Dafny\", \"HTML\", \"CSS\"};<br />')
-    .typeString('printf("%s\\n", me.info);<br />')
-    .typeString('$ I am currently a third year software engineering student at UQ. See my resume through the \'/about\' link at the top of the page for more info about me.<br /><br />')
-    .pauseFor(800)
-    .typeString('free_all_memory(&me);<br />')
-    .deleteAll(5)
-    .typeString('See my github: <br />')
-    .pauseFor(300)
-    .typeString('<a href="https://github.com/usrnatc">Github repos</a>')
+    .typeString('Person me = (Person *) malloc(sizeof(Person));<br />')
+    .typeString('me->age = 20;<br />')
+    .typeString('me->uni = (char *) malloc(BUFFERSIZE * sizeof(char));<br />')
+    .typeString('sprintf(me->uni, \"University of Queensland\\n\");<br />')
+    .typeString('me->langs = {\"Python\", \"Java\", \"C\", \"Haskell\", \"Dafny\", \"HTML\", \"CSS\"};<br />')
+    .typeString('printf("%s\\n", me->info);<br />')
+    .typeString('$ I am currently a third year software engineering student at UQ.<br />\
+		  &nbsp;&nbsp;I made this web-page to learn some simple front-end abailities with HTML, CSS, and trivial javascript.<br />\
+		  &nbsp;&nbsp;If you have any queries, concerns, and/or improvements to this web-page let me know <br />&nbsp;&nbsp;in an email through the contact link at the top of the page.<br /><br />\
+		  &nbsp;&nbsp;See my resume through the \'/about\' link at the top of the page for more info about me.<br /><br />')
