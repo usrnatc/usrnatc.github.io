bc4b1e28bab76066e5bc42d890d3472b35708a95
usrnatc <n.corcoran@uqconnect.edu.au>
2022-06-05 17:45:14 +1000

    refactor C code

--- terminal.js
+++ terminal.js
@@ -10,13 +10,15 @@ var typewriter = new Typewriter(app, {
 
 typewriter
     .typeString('#include "me.h"<br />')
-    .typeString('Person me = (Person *) malloc(sizeof(Person));<br />')
-    .typeString('me->age = 20;<br />')
-    .typeString('me->uni = (char *) malloc(BUFFERSIZE * sizeof(char));<br />')
-    .typeString('sprintf(me->uni, \"University of Queensland\\n\");<br />')
-    .typeString('me->langs = {\"Python\", \"Java\", \"C\", \"Haskell\", \"Dafny\", \"HTML\", \"CSS\"};<br />')
-    .typeString('printf("%s\\n", me->info);<br />')
+    .typeString('Me *nathan = malloc(sizeof(*nathan));<br />')
+    .typeString('nathan->age = 20;<br />')
+    .typeString('nathan->uni = (char *) malloc(STD_BUFFERSIZE * sizeof(char));<br />')
+    .typeString('sprintf(nathan->uni, \"University of Queensland\\n\");<br />')
+    .typeString('nathan->langs = {\"Python\", \"Java\", \"C\", \"Haskell\", \"Dafny\", \"HTML\", \"CSS\"};<br />')
+    .typeString('printf("%s\\n", nathan->info);<br />')
     .typeString('$ I am currently a third year software engineering student at UQ.<br />\
 		  &nbsp;&nbsp;I made this web-page to learn some simple front-end abilities with HTML, CSS, and trivial javascript.<br />\
 		  &nbsp;&nbsp;If you have any queries, concerns, and/or improvements to this web-page let me know <br />&nbsp;&nbsp;in an email through the \'/contact\' link at the top of the page.<br /><br />\
 		  &nbsp;&nbsp;See my resume through the \'/about\' link at the top of the page for more info about me.<br /><br />')
+    .typeString('free(nathan);<br />')
+    .typeString('nathan = NULL;')
